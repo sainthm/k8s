a08c615e29c3e87608ab5e4aba62c0511ed48f0e
sainthm <sainthm@naver.com>
2023-02-28 10:10:03 +0900

    Edit sh file

--- Study/Prometheus/fargate_validate_query.sh
+++ Study/Prometheus/fargate_validate_query.sh
@@ -1,5 +1,5 @@
 
-$ awscurl --region  --service aps "https://aps-workspaces.region_id.amazonaws.com/workspaces/workspace_id/api/v1/query?query=prometheus_api_remote_read_queries"
+$ awscurl --region  --service aps "https://aps-workspaces.us-west-2.amazonaws.com/workspaces/workspace_id/api/v1/query?query=prometheus_api_remote_read_queries"
 
 # {"status":"success","data":{"resultType":"vector","result":[{"metric":{"__name__":"prometheus_api_remote_read_queries","instance":"localhost:9090","job":"prometheus"},"value":[1648461236.419,"0"]}]}}21
       
